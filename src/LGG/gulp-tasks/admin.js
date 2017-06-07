05324b124d9a895d6804df386cd5d9dc81e55fee
Tuấn Lê Minh <bravohex@outlook.com>
2017-06-07 17:03:11 +0700

    Nhứng Text Editor

--- src/LGG/gulp-tasks/admin.js
+++ src/LGG/gulp-tasks/admin.js
@@ -10,113 +10,113 @@ var ts = require('gulp-typescript');
 var tsProject = ts.createProject('tsconfig.json');
 
 function clean() {
-  return del([
-    'temp/admin/**',
-    'wwwroot/admin/app/**'
-  ]);
+    return del([
+        'temp/admin/**',
+        'wwwroot/admin/app/**'
+    ]);
 }
 
 function styles() {
-  return gulp.src(['./dev/admin/app.scss'])
-    .pipe(sass({ outputStyle: 'compressed' })
-      .on('error', sass.logError))
-    .pipe(cleanCss({ keepSpecialComments: 0 }))
-    .pipe(gulp.dest('wwwroot/admin/'));
+    return gulp.src(['./dev/admin/app.scss'])
+        .pipe(sass({ outputStyle: 'compressed' })
+            .on('error', sass.logError))
+        .pipe(cleanCss({ keepSpecialComments: 0 }))
+        .pipe(gulp.dest('wwwroot/admin/'));
 }
 
 function componentStyles() {
-  return gulp.src(['./dev/admin/components/**/*.scss'])
-  .pipe(sass({ outputStyle: 'compressed' })
-    .on('error', sass.logError))
-  .pipe(cleanCss({ keepSpecialComments: 0 }))
-  .pipe(gulp.dest('wwwroot/admin/app/components'));
+    return gulp.src(['./dev/admin/components/**/*.scss'])
+        .pipe(sass({ outputStyle: 'compressed' })
+            .on('error', sass.logError))
+        .pipe(cleanCss({ keepSpecialComments: 0 }))
+        .pipe(gulp.dest('wwwroot/admin/app/components'));
 }
 
 function libs() {
-  return gulp.src([
-    'wwwroot/lib/jquery/dist/jquery.js',
-    'wwwroot/lib/foundation-sites/dist/js/plugins/foundation.core.js',
-    'wwwroot/lib/foundation-sites/dist/js/plugins/foundation.responsiveToggle.js',
-    'wwwroot/lib/foundation-sites/dist/js/plugins/foundation.util.mediaQuery.js'
-  ])
-      .pipe(concat('libs.js'))
-      .pipe(uglify())
-      .pipe(gulp.dest('wwwroot/admin/libs'));
+    return gulp.src([
+        'wwwroot/lib/jquery/dist/jquery.js',
+        'wwwroot/lib/foundation-sites/dist/js/plugins/foundation.core.js',
+        'wwwroot/lib/foundation-sites/dist/js/plugins/foundation.responsiveToggle.js',
+        'wwwroot/lib/foundation-sites/dist/js/plugins/foundation.util.mediaQuery.js'
+    ])
+        .pipe(concat('libs.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('wwwroot/admin/libs'));
 }
 
 function moveLibs() {
-  //var libs = {
-  //  "@angular": '@angular/**/*.js',
-  //  "systemjs": 'systemjs/dist/system.src.js',
-  //  "rxjs": 'rxjs/**/*.js',
-  //  "core-js": 'core-js/client/shim.min.js',
-  //  "zone.js": 'zone.js/dist/zone.js',
-  //  "reflect-metadata": 'reflect-metadata/Reflect.js'
-  //};
-
-  var libs = {
-    "@angular": '@angular/**/bundles/*.js',
-    "systemjs": 'systemjs/dist/system.src.js',
-    "rxjs": 'rxjs/**/*.js',
-    "core-js": 'core-js/client/shim.min.js',
-    "zone.js": 'zone.js/dist/zone.js'
-  };
-
-  for (var name in libs) {
-    if (libs.hasOwnProperty(name)) {
-      gulp.src('node_modules/' + libs[name])
-        .pipe(gulp.dest('wwwroot/admin/libs/' + name));
+    var libs = {
+        "@angular": '@angular/**/bundles/*.js',
+        "systemjs": 'systemjs/dist/system.src.js',
+        "rxjs": 'rxjs/**/*.js',
+        "core-js": 'core-js/client/shim.min.js',
+        "zone.js": 'zone.js/dist/zone.js',
+        "quill": 'quill/dist/quill.min.js'
+    };
+   
+    for (var name in libs) {
+        if (libs.hasOwnProperty(name)) {
+            gulp.src('node_modules/' + libs[name])
+                .pipe(gulp.dest('wwwroot/admin/libs/' + name));
+        }
     }
-  }
+    
+    return new Promise(function (resolve) {
+        resolve();
+    });
+}
 
-  return new Promise(function (resolve) {
-    resolve();
-  });
+function moveStyles() {
+    return gulp.src(['./node_modules/quill/dist/quill.snow.css'])
+        .pipe(cleanCss({ keepSpecialComments: 0 }))
+        .pipe(gulp.dest('wwwroot/admin/css'));
 }
 
 function scripts() {
-  return gulp.src([
-    'temp/admin/**/*'
-  ], { base: './temp/admin/' })
-      .pipe(gulp.dest('wwwroot/admin/app'));
+    return gulp.src([
+        'temp/admin/**/*'
+    ], { base: './temp/admin/' })
+        .pipe(gulp.dest('wwwroot/admin/app'));
 }
 
 function templates() {
-  return gulp.src([
-  'dev/admin/components/**/*.html'
-  ], { base: './dev/admin/components/' })
-      .pipe(gulp.dest('wwwroot/admin/app/components'));
+    return gulp.src([
+        'dev/admin/components/**/*.html'
+    ], { base: './dev/admin/components/' })
+        .pipe(gulp.dest('wwwroot/admin/app/components'));
 }
 
 function typescript() {
-  return tsProject.src()
-    .pipe(tsProject())
-    .pipe(gulp.dest('temp'));
+    return tsProject.src()
+        .pipe(tsProject())
+        .pipe(gulp.dest('temp'));
 }
 
 function watch() {
-  gulp.watch('./dev/admin/**/*.scss', styles);
-  gulp.watch('./dev/admin/components/**/*.html', templates);
-  gulp.watch('./dev/admin/**/*.ts', gulp.series(typescript, scripts));
+    gulp.watch('./dev/admin/**/*.scss', styles);
+    gulp.watch('./dev/admin/components/**/*.html', templates);
+    gulp.watch('./dev/admin/**/*.ts', gulp.series(typescript, scripts));
 }
 
 gulp.task('admin-dev', gulp.series(
-  clean,
-  moveLibs,
-  libs,
-  typescript,
-  scripts,
-  templates,
-  styles,
-  gulp.parallel(watch)
+    clean,
+    moveLibs,
+    moveStyles,
+    libs,
+    typescript,
+    scripts,
+    templates,
+    styles,
+    gulp.parallel(watch)
 ));
 
 gulp.task('admin', gulp.series(
-  clean,
-  moveLibs,
-  libs,
-  typescript,
-  scripts,
-  templates,
-  styles
+    clean,
+    moveLibs,
+    moveStyles,
+    libs,
+    typescript,
+    scripts,
+    templates,
+    styles
 ));
