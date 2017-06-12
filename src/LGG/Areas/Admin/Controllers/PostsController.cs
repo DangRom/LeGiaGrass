77041d22c29d9e91140a8a37b747b18897af5856
Tuấn Lê Minh <bravohex@outlook.com>
2017-06-12 16:45:37 +0700

    Remove Angular from Post. Working on client-side .... 80%

--- src/LGG/Areas/Admin/Controllers/PostsController.cs
+++ src/LGG/Areas/Admin/Controllers/PostsController.cs
@@ -1,4 +1,4 @@
-﻿#if (DEBUG)
+﻿using LGG.Core.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,13 +8,33 @@ namespace LGG.Areas.Admin.Controllers
     [Authorize]
     public class PostsController : Controller
     {
-        public IActionResult Index()
+        private readonly IPostService _postService;
+        private readonly ICategoryService _categoryService;
+        private readonly ITagService _tagService;
+
+        public PostsController(IPostService postService, ICategoryService categoryService, ITagService tagService)
+        {
+            _postService = postService;
+            _categoryService = categoryService;
+            _tagService = tagService;
+        }
+
+        public IActionResult Index(int? countPerPage,
+            int? currentPageIndex,
+            bool includeExceprt = true,
+            bool includeArticle = true,
+            bool includeUnpublished = true)
         {
             ViewBag.Title = "Admin | Posts";
             ViewBag.Selected = "posts";
-            ViewBag.SystemJsImportPath = "app/components/posts/main.js";
-            return View();
+            ViewBag.Categories = _categoryService.GetAll();
+            ViewBag.Tags = _tagService.GetAll();
+
+
+            if (countPerPage == null || currentPageIndex == null)
+                return View(_postService.GetAll(includeExceprt, includeArticle, includeUnpublished));
+            else
+                return View(_postService.GetAllPaged((int)countPerPage, (int)currentPageIndex, includeUnpublished));
         }
     }
 }
-#endif
